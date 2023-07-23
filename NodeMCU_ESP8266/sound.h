8280213601ba1af622bae6ed87769a5ee1ac1254
jinlijennie <NGCzjy2237@163.com>
2023-07-23 14:11:12 +0800

    second commit

---
+++ NodeMCU_ESP8266/sound.h
@@ -0,0 +1,9 @@
+//此处修改语音数据，MR628语音合成协议为<G>开头，
+//所以只需修改<G>后的内容，请勿删除<G>或更改其位置
+
+const char sound[]={"<G>大家好我是语音播报小助手徐恬"};
+const char soundtemp[]={"<G>温度超过阈值"}；
+const char soundhum[]={"<G>湿度超过阈值"}；
+const char soundco2[]={"<G>二氧化碳含量超过阈值"}；
+const char soundsun[]={"<G>光照强度超过阈值"}；
+const char soundtvoc[]={"<G>有毒有害气体浓度超过阈值"}；
